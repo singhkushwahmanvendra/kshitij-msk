63d18cfe36f3c2de347d4e5146803a7d471561e3
singhkushwahmanvendra <50552278+singhkushwahmanvendra@users.noreply.github.com>
2019-05-26 12:29:52 +0530

    Add files via upload

--- snake.js
+++ snake.js
@@ -1,10 +1,19 @@
 ﻿var cvs = document.getElementById("canvas");
 var ctx = document.getElementById("canvas").getContext("2d");
+
 var snakeW = 20;
 var snakeH = 20;
 var len, snake, snakeX, snakeY,id,score=0,eat,dead,left,right,down;
 var dir = "right",mov="manual";
-var food,n=0,Sspeed=0,p=1;
+var food, n = 0, Sspeed = 0, p = 1;
+
+var visited;
+
+visited = []
+
+var maze;
+
+maze = [];
 
 eat = document.getElementById("eat");
 dead = document.getElementById("dead");
@@ -41,8 +50,8 @@ if (mov == "manual") {
 }
 
 food = {
-    x: Math.round(Math.random() * (cvs.width / snakeW - 2) ),
-    y: Math.round(Math.random() * (cvs.height / snakeH - 2) )
+    x: Math.round(Math.random() * ((cvs.width / snakeW) -2)+1),
+    y: Math.round(Math.random() * ((cvs.height / snakeH )-2)+1 )
 }
 
 function drawFood(x, y) {
@@ -100,7 +109,11 @@ function draw() {                                  //drawing snake function.
 
     snake.unshift({ x: snakeX, y: snakeY });                                //saving new block to snake by unshift method.
 
-    drawFood(food.x, food.y);                                          //draw food on canvas.
+    drawFood(food.x, food.y);                                            //draw food on canvas.
+
+   
+       // mazeConstruct();
+    
 
     for (var i = 1; i < snake.length; i++) {                                                 //when snake bites itself.
         if (snakeX == snake[i].x && snakeY == snake[i].y) {
@@ -112,7 +125,7 @@ function draw() {                                  //drawing snake function.
     }
     
 
-    if (snakeX < 0 || snakeY < 0 || snakeX > (cvs.width / snakeW) || snakeY > (cvs.height / snakeH)) {
+    if (snakeX < 0 || snakeY < 0 || snakeX > (cvs.width / snakeW)-1 || snakeY > (cvs.height / snakeH)-1) {
         if (n > 1) {
             dead.play();
             clearInterval(id);
@@ -148,8 +161,8 @@ function tscore() {
 
 function auto() {
 
-    if (snakeX <= food.x && snakeY <= food.y) {
-        while (snakeY < food.y) {
+    if (snakeX <= food.x && snakeY <= food.y && dir != "up") {
+        while (snakeY < food.y ) {
             dir = "down";
             return;
         }
@@ -159,7 +172,7 @@ function auto() {
         }
      }
 
-    if (snakeX <= food.x && snakeY >= food.y) {
+    if (snakeX <= food.x && snakeY >= food.y && dir != "down") {
         while (snakeY > food.y) {
             dir = "up";
             return;
@@ -170,7 +183,7 @@ function auto() {
         }
     }
 
-    if (snakeX >= food.x && snakeY >= food.y) {
+    if (snakeX >= food.x && snakeY >= food.y && dir != "down") {
         while (snakeY > food.y) {
             dir = "up";
             return;
@@ -181,7 +194,7 @@ function auto() {
         }
     }
 
-    if (snakeX >= food.x && snakeY <= food.y) {
+    if (snakeX >= food.x && snakeY <= food.y && dir != "up") {
         while (snakeY < food.y) {
             dir = "down";
             return;
@@ -194,7 +207,7 @@ function auto() {
 
                                                       //if dir is up or down.
 
-    if (snakeX <= food.x && snakeY <= food.y) {
+    if (snakeX <= food.x && snakeY <= food.y && dir != "left") {
         while (snakeX < food.x) {
             dir = "right";
             return;
@@ -205,7 +218,7 @@ function auto() {
         }      
     }
 
-    if (snakeX <= food.x && snakeY >= food.y) {
+    if (snakeX <= food.x && snakeY >= food.y && dir != "left") {
         while (snakeX < food.x) {
             dir = "right";
             return;
@@ -216,7 +229,7 @@ function auto() {
         }
     }
 
-    if (snakeX >= food.x && snakeY >= food.y) {
+    if (snakeX >= food.x && snakeY >= food.y && dir != "right") {
         while (snakeX > food.x) {
             dir = "left";
             return;
@@ -228,7 +241,7 @@ function auto() {
        
     }
 
-    if (snakeX >= food.x && snakeY <= food.y) {
+    if (snakeX >= food.x && snakeY <= food.y && dir != "right") {
         while (snakeX > food.x) {
             dir = "left";
             return;
@@ -247,21 +260,21 @@ function auto() {
 
 
 
-    if (snakeX >= food.x && snakeY == food.y) {
+    if (snakeX >= food.x && snakeY == food.y && dir != "down") {
         
             dir = "up;"
             return;
     }
-    if (snakeX == food.x && snakeY <= food.y) {
+    if (snakeX == food.x && snakeY <= food.y && dir != "right") {
         dir = "left";
         return;
     }
-    if (snakeX <= food.x && snakeY == food.y) {
+    if (snakeX <= food.x && snakeY == food.y && dir != "down") {
 
         dir = "up";
         return;
     }
-    if (snakeX == food.x && snakeY >= food.y) {
+    if (snakeX == food.x && snakeY >= food.y && dir != "right") {
         dir = "left";
         return;
     }
@@ -285,3 +298,97 @@ while (snakeX >= food.x) {
 while (snakeY >= food.y) {
     dir = "up";
 }*/
+
+
+
+function mazeConstruct()
+{
+    maze.push({
+        x: 0,
+        y: 0
+    })
+    visited.push({
+        x: 0,
+        y: 0
+    })
+
+    for (var i = 0; i < maze.length; i++) {
+        maze.pop();
+    }
+    for (var i = 0; i < visited.length; i++) {
+        visited.pop();
+    }
+
+    maze.push({
+        x: snakeX,
+        y: snakeY
+    })
+
+    var  mazex, mazey;
+
+    for (var i = 0; i > -1; i++) {
+
+        mazex = maze[maze.length-1].x;
+        mazey = maze[maze.length-1].y;
+
+        if (mazex == food.x && mazey == food.y) {
+            return;
+            break;
+        }
+
+        if (visit(mazex + 1, mazey)==1) {
+            maze.push({ x: mazex + 1,y: mazey });
+
+        }
+
+        else if (visit(mazex, mazey + 1)==1) {
+            maze.push({ x: mazex, y: mazey + 1 });
+         
+        }
+
+        else if (visit(mazex, mazey - 1)==1){
+            maze.push({ x: mazex, y: mazey - 1 });
+          
+        }
+
+        else if (visit(mazex - 1, mazey)==1) {
+            maze.push({ x: mazex - 1, y: mazey });
+           
+        }
+
+        else{
+            visited.push({
+                x: maze[maze.length-1].x,
+                y: maze[maze.length-1].y
+            })
+            maze.pop();
+        }
+    }
+    return;
+}
+
+function visit(x, y) {
+
+    for (var i = 0; i < visited.length; i++) {
+
+        if (x == visited[i].x && y == visited[i].y) {
+            return 0;
+        }
+    }
+
+    for (var i = 0; i < snake.length; i++) {
+
+        if (x == snake[i].x && y == snake[i].y) {
+            return 0;
+        }
+    }
+
+    if (x < 0 || y < 0 || x > (cvs.width / snakeW) || y > (cvs.height / snakeH)) {
+        if (n > 1) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+
